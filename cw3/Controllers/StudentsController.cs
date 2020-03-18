f3b36d7a734f8e86517e9fb121000750fdc6f7db
urbanskik20@gmail.com <urbanskik20@gmail.com>
2020-03-18 19:44:59 +0100

    AllProject

---
+++ cw3/Controllers/StudentsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Cw3.DAL;
+using Cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        [HttpGet()]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if(id == 1)
+            {
+                return Ok("Tolin");
+            }
+            else if(id == 2)
+            {
+                return Ok("Tybrel");
+            }
+            else if (id == 3)
+            {
+                return Ok("Dyrdun");
+            }
+            return NotFound("Nie znaleziono studenta...");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 2000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {         
+            return Ok("Student zaktualizowany");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            //co tu dodac bez bazy danych?
+            return Ok("Student usunięty");
+        }
+    }
+}
---
+++ cw3/DAL/MockDbService.cs
@@ -0,0 +1,25 @@
+﻿using Cw3.Models;
+using System.Collections.Generic;
+
+namespace Cw3.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student { IdStudent = 1, FirstName = "Miłosz", LastName = "Sosnowski" },
+                new Student { IdStudent = 2, FirstName = "Kuba", LastName = "Zagajewski" },
+                new Student { IdStudent = 3, FirstName = "Michał", LastName = "Ferendowicz" }
+            };
+        }
+
+            public IEnumerable<Student> GetStudents()
+            {
+            return _students;
+            }
+        }
+    }
